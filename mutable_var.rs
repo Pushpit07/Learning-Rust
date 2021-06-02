543ce861feb7d77301866b8fbf1236d521c8c992
Pushpit <pushpit07@gmail.com>
2021-06-02 10:07:45 +0530

    Create mutable_var.rs

---
+++ mutable_var.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let name = "Pushpit";
+    let mut age = 19;
+    println!("My name is {} and I am {}", name, age);
+
+    age = 20;
+    println!("My name is {} and I am {}", name, age);
+}
